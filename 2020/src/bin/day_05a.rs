3a60f6f4fc5a741942fe1bc6ed4ad646efe43055
Andrés Medina <andresnrx@gmail.com>
2020-12-04 21:46:42 -0800

    2020 - 5a

---
+++ 2020/src/bin/day_05a.rs
@@ -0,0 +1,68 @@
+use bitvec::prelude::{bitarr, Lsb0};
+
+fn solve(seats: &str) -> u32 {
+    seats.trim().lines().map(cost_id).max().unwrap()
+}
+
+fn cost_id(seat: &str) -> u32 {
+    let row = seat[0..7]
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(bitarr![Lsb0, u32; 0; 8], |mut column, (i, c)| {
+            match c {
+                'F' => {}
+                'B' => column.set(i, true),
+                c => panic!("unexpected: {:?}", c),
+            };
+            column
+        })
+        .as_slice()[0];
+
+    let column = seat[7..10]
+        .chars()
+        .rev()
+        .enumerate()
+        .fold(bitarr![Lsb0, u32; 0; 3], |mut column, (i, c)| {
+            match c {
+                'L' => {}
+                'R' => column.set(i, true),
+                c => panic!("unexpected: {:?}", c),
+            };
+            column
+        })
+        .as_slice()[0];
+
+    row * 8 + column
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_one() {
+        let input = r"FBFBBFFRLR";
+        assert_eq!(solve(input), 357);
+    }
+
+    #[test]
+    fn example_two() {
+        let input = r"BFFFBBFRRR";
+        assert_eq!(solve(input), 567);
+    }
+
+    #[test]
+    fn example_three() {
+        let input = r"FFFBBBFRRR";
+        assert_eq!(solve(input), 119);
+    }
+
+    #[test]
+    fn example_four() {
+        let input = r"BBFFBBFRLL";
+        assert_eq!(solve(input), 820);
+    }
+}
+
+common::read_main!();
