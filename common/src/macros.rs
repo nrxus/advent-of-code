c0f31956505f4822f9018f5a13ed36c989baaab3
Andrés Medina <andresnrx@gmail.com>
2020-12-16 09:42:35 -0800

    output elapsed time

--- common/src/macros.rs
+++ common/src/macros.rs
@@ -18,7 +18,21 @@ macro_rules! read_main {
 
             let mut input = String::new();
             std::io::stdin().read_to_string(&mut input).unwrap();
-            println!("{}", solve(input.as_str()));
+            let input = input.as_str();
+            let start = std::time::Instant::now();
+            let solution = solve(input);
+            let elapsed = start.elapsed();
+            let seconds = elapsed.as_secs();
+            let millis = elapsed.subsec_millis();
+            let micros = elapsed.subsec_micros() - millis * 1000;
+
+            println!(
+                "{}\n\nfinished in: {seconds}s {ms}.{us}ms",
+                solution,
+                seconds = seconds,
+                ms = millis,
+                us = micros
+            );
         }
     };
 }
