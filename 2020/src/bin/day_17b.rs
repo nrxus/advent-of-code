98dd54c4a7c19ae37973f21d585003d3e2f3157a
Andrés Medina <andresnrx@gmail.com>
2020-12-18 10:45:58 -0800

    use symmetry to cut run time by ~70%.

--- 2020/src/bin/day_17b.rs
+++ 2020/src/bin/day_17b.rs
@@ -13,7 +13,7 @@ fn solve(initial: &str) -> usize {
         })
         .collect();
 
-    let mut min = (0, 0, 0, 0);
+    let mut min = (0_i32, 0_i32, 0_i32, 0_i32);
     let mut max = (0, 0, 0, 0);
 
     max.0 = actives.iter().map(|p| p.0).max().unwrap();
@@ -24,10 +24,10 @@ fn solve(initial: &str) -> usize {
     (0..6).for_each(|_| {
         actives = (min.0 - 1..=max.0 + 1)
             .flat_map(|x| (min.1 - 1..=max.1 + 1).map(move |y| (x, y)))
-            .flat_map(|(x, y)| (min.2 - 1..=max.2 + 1).map(move |z| (x, y, z)))
-            .flat_map(|(x, y, z)| (min.3 - 1..=max.3 + 1).map(move |w| (x, y, z, w)))
+            .flat_map(|(x, y)| (0_i32..=max.2 + 1).map(move |z| (x, y, z)))
+            .flat_map(|(x, y, z)| (0_i32..=max.3 + 1).map(move |w| (x, y, z, w)))
             .filter_map(|(x, y, z, w)| {
-                let active_count = neighbors((x, y, z, w))
+                let active_count = neighbors((x, y, z.abs(), w.abs()))
                     .filter(|p| actives.contains(p))
                     .take(4)
                     .count();
@@ -45,18 +45,18 @@ fn solve(initial: &str) -> usize {
         max.3 = actives.iter().map(|p| p.3).max().unwrap();
         min.0 = actives.iter().map(|p| p.0).min().unwrap();
         min.1 = actives.iter().map(|p| p.1).min().unwrap();
-        min.2 = actives.iter().map(|p| p.2).min().unwrap();
-        min.3 = actives.iter().map(|p| p.3).min().unwrap();
     });
 
-    actives.len()
+    let not_mirrored = actives.iter().filter(|(_,_,z,w)| *z == 0 && *w == 0).count();
+    let mirrored_once = actives.iter().filter(|(_,_,z,w)| (*z == 0) ^ (*w == 0)).count();
+    actives.len() * 4 - 3 * not_mirrored - 2 * mirrored_once
 }
 
 fn neighbors((x, y, z, w): (i32, i32, i32, i32)) -> impl Iterator<Item = (i32, i32, i32, i32)> {
     (x - 1..=x + 1)
         .flat_map(move |x| (y - 1..=y + 1).map(move |y| (x, y)))
-        .flat_map(move |(x, y)| (z - 1..=z + 1).map(move |z| (x, y, z)))
-        .flat_map(move |(x, y, z)| (w - 1..=w + 1).map(move |w| (x, y, z, w)))
+        .flat_map(move |(x, y)| (z - 1..=z + 1).map(move |z| (x, y, z.abs())))
+        .flat_map(move |(x, y, z)| (w - 1..=w + 1).map(move |w| (x, y, z, w.abs())))
         .filter(move |p| *p != (x, y, z, w))
 }
 
