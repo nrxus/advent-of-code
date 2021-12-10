5782d2daa5dd054a716884f54bfaeeda4571ae69
Andrés Medina <andresnrx@gmail.com>
2021-12-09 22:20:47 -0800

    day 10 - adding types because I am bored

--- 2021/src/bin/day_10b.rs
+++ 2021/src/bin/day_10b.rs
@@ -2,50 +2,90 @@ fn solve(input: &str) -> u64 {
     let mut scores: Vec<_> = input
         .trim()
         .lines()
-        .filter_map(|line| {
-            let mut stack = vec![];
-
-            for c in line.chars() {
-                let expected = match c {
-                    '(' | '{' | '[' | '<' => {
-                        stack.push(c);
-                        continue;
-                    }
-                    ')' => '(',
-                    ']' => '[',
-                    '}' => '{',
-                    '>' => '<',
-                    e => panic!("unexpected char: {}", e),
-                };
-
-                let opener = stack
-                    .pop()
-                    .expect("closer should never appear before opener");
-
-                if opener != expected {
-                    return None;
-                }
-            }
-
-            let score = stack.into_iter().rev().fold(0, |score, opener| {
-                let points = match opener {
-                    '(' => 1,
-                    '[' => 2,
-                    '{' => 3,
-                    '<' => 4,
-                    _ => unreachable!(),
-                };
-
-                score * 5 + points
-            });
-
-            Some(score)
+        .map(|line| line.chars().map(|c| Brace::from_char(c).unwrap()))
+        .filter_map(Brace::find_incomplete)
+        .map(|unpaired| {
+            unpaired
+                .into_iter()
+                .rev()
+                .fold(0, |score, opener| score * 5 + opener.points())
         })
         .collect();
 
     scores.sort_unstable();
 
     scores[scores.len() / 2]
+}
+
+#[derive(Clone, Copy)]
+enum Opener {
+    Paren = 1,
+    Bracket = 2,
+    Curly = 3,
+    Angled = 4,
+}
+
+#[derive(Clone, Copy)]
+enum Closer {
+    Paren,
+    Bracket,
+    Curly,
+    Angled,
+}
+
+enum Brace {
+    Opener(Opener),
+    Closer(Closer),
+}
+
+impl Brace {
+    pub fn from_char(c: char) -> Result<Self, Box<dyn std::error::Error>> {
+        let brace = match c {
+            '(' => Brace::Opener(Opener::Paren),
+            '{' => Brace::Opener(Opener::Curly),
+            '[' => Brace::Opener(Opener::Bracket),
+            '<' => Brace::Opener(Opener::Angled),
+            ')' => Brace::Closer(Closer::Paren),
+            ']' => Brace::Closer(Closer::Bracket),
+            '}' => Brace::Closer(Closer::Curly),
+            '>' => Brace::Closer(Closer::Angled),
+            e => return Err(format!("invalid brace char: {}", e).into()),
+        };
+
+        Ok(brace)
+    }
+
+    pub fn find_incomplete(mut braces: impl Iterator<Item = Self>) -> Option<Vec<Opener>> {
+        braces.try_fold(vec![], |mut openers, brace| {
+            match brace {
+                Brace::Opener(opener) => openers.push(opener),
+                Brace::Closer(closer) => {
+                    let opener = openers.pop()?;
+                    if !opener.matchers_closer(closer) {
+                        return None;
+                    }
+                }
+            }
+
+            Some(openers)
+        })
+    }
+}
+
+impl Opener {
+    pub fn points(self) -> u64 {
+        self as u64
+    }
+
+    pub fn matchers_closer(self, closer: Closer) -> bool {
+        matches!(
+            (self, closer),
+            (Opener::Paren, Closer::Paren)
+                | (Opener::Bracket, Closer::Bracket)
+                | (Opener::Curly, Closer::Curly)
+                | (Opener::Angled, Closer::Angled)
+        )
+    }
 }
 
 #[cfg(test)]
