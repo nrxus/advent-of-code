1ccdcfb9cd8dc2452ead2a6f7b5fd15134018d6f
Andrés Medina <andresnrx@gmail.com>
2021-12-10 23:18:16 -0800

    add octopus type because why not

--- 2021/src/bin/day_11b.rs
+++ 2021/src/bin/day_11b.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashSet,
-    fmt::{self, Formatter},
+    error::Error,
+    fmt::{self, Formatter, Write},
     str::FromStr,
 };
 
@@ -12,47 +13,43 @@ fn solve(input: &str) -> usize {
 
 #[derive(Debug)]
 struct Cavern {
-    grid: [u8; 100],
+    grid: [Octopus; 100],
 }
 
 impl Cavern {
     const LEN: usize = 10;
 
     pub fn run_step(&mut self) -> usize {
-        self.grid.iter_mut().for_each(|v| *v = (*v + 1) % 10);
-
         let mut frontier: Vec<usize> = self
             .grid
-            .iter()
+            .iter_mut()
             .enumerate()
-            .filter_map(|(i, v)| Some(i).filter(|_| *v == 0))
+            .filter_map(|(i, v)| if v.power_up() { Some(i) } else { None })
             .collect();
 
         let mut flashed: HashSet<usize> = frontier.iter().copied().collect();
 
         while let Some(i) = frontier.pop() {
-            let flasing_neighbors: Vec<_> = self
+            let flashing_neighbors: Vec<_> = self
                 .neighbor_indices(i)
                 .into_iter()
-                .flatten()
                 .filter(|&i| {
                     if flashed.contains(&i) {
                         return false;
                     }
 
-                    self.grid[i] = (self.grid[i] + 1) % 10;
-                    self.grid[i] == 0
+                    self.grid[i].power_up()
                 })
                 .collect();
 
-            flashed.extend(flasing_neighbors.iter().copied());
-            frontier.extend(flasing_neighbors);
+            flashed.extend(flashing_neighbors.iter().copied());
+            frontier.extend(flashing_neighbors);
         }
 
         flashed.len()
     }
 
-    fn neighbor_indices(&self, index: usize) -> [Option<usize>; 8] {
+    fn neighbor_indices(&self, index: usize) -> impl Iterator<Item = usize> {
         let left = if index % Self::LEN == 0 {
             None
         } else {
@@ -69,6 +66,26 @@ impl Cavern {
         [
             left, right, up, down, up_left, up_right, down_left, down_right,
         ]
+        .into_iter()
+        .flatten()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Octopus(u8);
+
+impl Octopus {
+    const MAX_POWER: u8 = 10;
+
+    pub fn new(c: char) -> Result<Self, Box<dyn Error>> {
+        c.to_digit(10)
+            .map(|d| Self(d as u8))
+            .ok_or_else(|| "octopus has to be a digit".to_owned().into())
+    }
+
+    pub fn power_up(&mut self) -> bool {
+        self.0 = (self.0 + 1) % Self::MAX_POWER;
+        self.0 == 0
     }
 }
 
@@ -76,17 +93,17 @@ impl std::str::FromStr for Cavern {
     type Err = Box<dyn std::error::Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut grid = [0; Self::LEN * Self::LEN];
+        let mut grid = [Octopus(0); Self::LEN * Self::LEN];
         s.trim()
             .lines()
             .flat_map(|s| s.chars())
             .enumerate()
-            .try_for_each(|(i, g)| match g.to_digit(10) {
-                Some(d) => {
-                    grid[i] = d as u8;
+            .try_for_each(|(i, g)| match Octopus::new(g) {
+                Ok(o) => {
+                    grid[i] = o;
                     Ok(())
                 }
-                None => Err("input character is not a digit".to_string()),
+                Err(e) => Err(e),
             })?;
 
         Ok(Self { grid })
@@ -95,9 +112,10 @@ impl std::str::FromStr for Cavern {
 
 impl fmt::Display for Cavern {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        self.grid
-            .chunks_exact(Self::LEN)
-            .try_for_each(|line| writeln!(f, "{:?}", line))
+        self.grid.chunks_exact(Self::LEN).try_for_each(|line| {
+            line.iter().try_for_each(|o| write!(f, "{}", o.0))?;
+            f.write_str("\n")
+        })
     }
 }
 
