fdea6a2cf1ebf4a60f89f09c0b1eb5b41bddd565
Andrés Medina <andresnrx@gmail.com>
2022-12-11 00:06:27 -0800

    2022 day 11

---
+++ 2022/src/bin/day_11b.rs
@@ -0,0 +1,135 @@
+use common::read_main;
+
+fn solve(input: &str) -> usize {
+    let mut max_worry = 1;
+
+    let mut monkeys: Vec<Monkey> = input
+        .trim()
+        .split("\n\n")
+        .map(|raw_monkey| {
+            let mut raw_monkey = raw_monkey.lines().skip(1); // Monkey N
+            let mut next_stripped = |prefix: &str| {
+                raw_monkey
+                    .next()
+                    .unwrap()
+                    .trim()
+                    .strip_prefix(prefix)
+                    .unwrap()
+            };
+            let starting = next_stripped("Starting items: ");
+            let worries = starting.split(", ").map(|w| w.parse().unwrap()).collect();
+
+            let operation = next_stripped("Operation: new = old ");
+            let operation: Box<dyn Fn(_) -> _> = match operation {
+                "* old" => Box::new(|old| old * old),
+                "+ old" => Box::new(|old| old + old),
+                _ => {
+                    let (operation, operand) = operation.split_once(' ').unwrap();
+                    let operand: u64 = operand.parse().unwrap();
+                    match operation {
+                        "*" => Box::new(move |old| old * operand),
+                        "+" => Box::new(move |old| old + operand),
+                        op => panic!("unhandled operation: {op}"),
+                    }
+                }
+            };
+
+            let test: u64 = next_stripped("Test: divisible by ").parse().unwrap();
+            max_worry *= test;
+            let if_true = next_stripped("If true: throw to monkey ").parse().unwrap();
+            let if_false = next_stripped("If false: throw to monkey ").parse().unwrap();
+            let test = Box::new(
+                move |worry| {
+                    if worry % test == 0 {
+                        if_true
+                    } else {
+                        if_false
+                    }
+                },
+            );
+
+            Monkey {
+                worries,
+                operation,
+                test,
+                num_inspections: 0,
+            }
+        })
+        .collect();
+
+    (0..10_000).for_each(|_| {
+        (0..monkeys.len()).for_each(|monkey_index| {
+            while let Some(worry) = monkeys[monkey_index].inspect() {
+                let worry = worry % max_worry;
+                let throw_to = monkeys[monkey_index].test(worry);
+                monkeys[throw_to].receive(worry);
+            }
+        })
+    });
+
+    monkeys.sort_by_key(|m| m.num_inspections);
+    monkeys.pop().unwrap().num_inspections * monkeys.pop().unwrap().num_inspections
+}
+
+struct Monkey {
+    worries: Vec<u64>,
+    operation: Box<dyn Fn(u64) -> u64>,
+    test: Box<dyn Fn(u64) -> usize>,
+    num_inspections: usize,
+}
+impl Monkey {
+    fn inspect(&mut self) -> Option<u64> {
+        self.worries.pop().map(|old| {
+            self.num_inspections += 1;
+            (self.operation)(old)
+        })
+    }
+
+    fn test(&self, worry: u64) -> usize {
+        (self.test)(worry)
+    }
+
+    fn receive(&mut self, worry: u64) {
+        self.worries.push(worry)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input = r"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+";
+        assert_eq!(solve(input), 2713310158);
+    }
+}
+
+read_main!();
