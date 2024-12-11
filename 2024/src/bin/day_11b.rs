2877ebc7702491799230d80c455a27f6adccb381
Andrés Medina <andresnrx@gmail.com>
2024-12-10 21:47:29 -0800

    2024 - day 11

---
+++ 2024/src/bin/day_11b.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+fn solve(input: &str) -> usize {
+    let stones: HashMap<u64, usize> = input
+        .trim()
+        .split_whitespace()
+        .map(|stone| stone.parse::<u64>().unwrap())
+        .fold(HashMap::new(), |mut stones, stone| {
+            *stones.entry(stone).or_insert(0) += 1;
+            stones
+        });
+
+    let stones = (0..75).fold(stones, |stones, _| {
+        stones
+            .into_iter()
+            .fold(HashMap::new(), |mut stones, (stone, count)| {
+                if stone == 0 {
+                    *stones.entry(1).or_insert(0) += count;
+                } else {
+                    let stone_str = format!("{stone}");
+                    if stone_str.len() % 2 == 0 {
+                        let (left, right) = stone_str.split_at(stone_str.len() / 2);
+                        *stones.entry(left.parse().unwrap()).or_insert(0) += count;
+                        *stones.entry(right.parse().unwrap()).or_insert(0) += count;
+                    } else {
+                        *stones.entry(stone * 2024).or_insert(0) += count;
+                    }
+                }
+
+                stones
+            })
+    });
+
+    stones.into_values().sum()
+}
+
+common::read_main!();
